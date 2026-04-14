3bd2308848e8f2baa2109b8c36cd559bece7ff6a
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-14 15:51:42 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,15 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { app } from "./firebase-config.js";
+
+const auth = getAuth(app);
+
+window.login = function() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+};
