e6253b83f618e3a17f89b1638f6c791e75623d93
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-15 18:03:53 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-export const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-};
-
-export const app = initializeApp(firebaseConfig);
